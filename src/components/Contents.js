4316af843b6fe5ed98eaa7a94c227883f4eec2a9
csslick <digitone@naver.com>
2021-04-01 21:24:43 +0900

    bar graph

--- src/components/Contents.js
+++ src/components/Contents.js
@@ -3,7 +3,8 @@ import axios from "axios";
 import { Bar, Doughnut, Line } from "react-chartjs-2";
 
 export default function Contents() {
-  const [data, setData] = useState([{
+
+  const [data, setData] = useState({
     labels: ["1월", "2월", "3월"],
     datasets: [
       {
@@ -13,40 +14,45 @@ export default function Contents() {
         data: [10, 5, 3, 0],
       }
     ],
-  }]);
+  });
 
   useEffect(() => {
-  //   async function fetch_data() {
-  //     const res = await axios.get('https://api.covid19api.com/total/dayone/country/kr')
-  //     console.log(res.data);
-  //   }
-  //   fetch_data();
-
     function makeData(items) {
       const _data = [];
-      let prev_month = -1; // 지난 달 초기값
 
+      // 날짜 데이터 가공
       items.forEach(item => {
         const year = Number(item.Date.split('-')[0]);
-        const cur_month = Number(item.Date.split('-')[1]);
+        const month = Number(item.Date.split('-')[1]);
         const date = Number(item.Date.split('-')[2].slice(0,2));
-        const last_date = Number(new Date(year, cur_month, 0).getDate());
+        const last_date = Number(new Date(year, month, 0).getDate());
        
-        // 월별 말일만 검색(같은 월이 아닐때)
-        if(prev_month !== cur_month) {
-          // 말일이 아니면 제외
-          if(date == last_date){
-            console.log('last date= ', last_date)
-            _data.push({
-              year: year,
-              month: cur_month,
-              date: date,
-              confirmed: item.Confirmed,
-            });
-          }         
-        }
+        // 월별 말일 데이터만 추출(누적 확진자 데이터만 필요)
+        if(date === last_date){
+          console.log('last date= ', last_date)
+          _data.push({
+            year: year,
+            month: month + '월',
+            date: date,
+            confirmed: item.Confirmed,
+          });
+        }         
+
       });
-      console.log(_data)
+
+      const labels = _data.map(d => d.year + ' ' + d.month) // 월
+      setData({
+        labels: labels,
+        datasets: [
+          {
+            label: "국내 누적 확진자",
+            backgroundColor: "#E82B26",
+            fill: true,
+            data: _data.map(d => d.confirmed), //  누적확진자
+          }
+        ],
+      });
+      console.log(data)
     }
 
     axios
@@ -60,7 +66,6 @@ export default function Contents() {
         console.log(error);
       })  
 
-
   }, []);
 
   return (
@@ -68,7 +73,7 @@ export default function Contents() {
       <h2>국내 코로나 현황</h2>
       <figure className="chart">
         <Bar 
-          data={data[0]}
+          data={data}
           options={
             {
               title: {display: true, text:"누적 확진자 추이", fontSize: 16}, 
